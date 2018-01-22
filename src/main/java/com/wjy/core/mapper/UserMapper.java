29bb6e93384fb4820c5909fe15c30df554aca14c
wjy <wjy@wjydeMacBook-Pro.local>
2018-01-22 14:45:14 +0800

    fix

--- src/main/java/com/wjy/core/mapper/UserMapper.java
+++
@@ -1,8 +0,0 @@
-package com.wjy.core.mapper;
-
-import com.wjy.core.domain.User;
-
-public interface UserMapper {
-
-    User selectUser(long id);
-}
--- src/main/java/com/wjy/core/service/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.wjy.core.service;
-
-import com.wjy.core.domain.User;
-
-public interface IUserService {
-
-    public User selectUser(long userId);
-
-}
